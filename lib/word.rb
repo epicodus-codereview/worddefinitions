3093e5017ed231afcf961f7c569375a57b6cc0dc
Jing Wait <jingzhaowait@gmail.com>
2015-08-14 12:24:31 -0700

    fetch the searching word pass spec

--- lib/word.rb
+++ lib/word.rb
@@ -1,8 +1,7 @@
 class Word
   @@words = []
-  def initialize(name, definition)
+  def initialize(name)
     @name = name
-    @definition = definition
 
   end
 
@@ -10,12 +9,26 @@ class Word
     @name
   end
 
-  def definition
-    @definition
-  end
-
   def self.all_words
     @@words
+  end
+
+  def save
+    @@words.push(self)
+  end
+
+  def self.clear
+    @@words = []
+  end
+
+  def self.find(name)
+    found_word = nil
+    @@words.each do |word|
+      if word.name.eql?(name)
+        found_word = word
+      end
+    end
+    found_word
   end
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,34 +3,61 @@ require('./lib/word')
 
 
 describe Word do
-  #before do
-    #Word.clear
-  #end
-
-  describe ('initialize') do
-    it ('initialize instance for word class') do
-      test_word = Word.new("apple", "a type of fruit")
-      expect(test_word.name).to(eq("apple"))
-    end
+  before do
+    Word.clear
+    @test_word = Word.new('pasta')
   end
+
+
+
+  # describe ('initialize') do
+  #   it ('initialize instance for word class') do
+  #     #test_word = Word.new("apple", "a type of fruit")
+  #     expect(@test_word).to(eq("pasta"))
+  #   end
+  # end
 
   describe ('#name') do
     it("returns the name of word") do
-      test_word = Word.new("lab", "a breed of dog")
-      expect(test_word.name).to(eq("lab"))
+      #test_word = Word.new("lab", "a breed of dog")
+      expect(@test_word.name).to(eq("pasta"))
     end
   end
 
-  describe ('#definition') do
-    it('returns the definition of word') do
-      test_word = Word.new('lab', 'a breed of dog')
-      expect(test_word.definition).to(eq("a breed of dog"))
-    end
-  end
+  # describe ('#definition') do
+  #   it('returns the definition of word') do
+  #     #test_word = Word.new('lab', 'a breed of dog')
+  #     expect(@test_word.definition).to(eq("a type of food"))
+  #   end
+  # end
 
   describe ('#words') do
     it ('returns an empty array of words') do
       expect(Word.all_words).to eq []
+    end
+  end
+
+  describe ('#save') do
+    it 'adds a new word to the array of words' do
+      @test_word.save
+      expect(Word.all_words).to eq [@test_word]
+    end
+  end
+
+  describe '.clear' do
+    it 'empties out all the saved words' do
+      Word.clear
+      expect(Word.all_words).to eq []
+    end
+  end
+
+  describe '.find' do
+    it 'returns a word by matching the given word name' do
+      @test_word.save
+      test_word1 = Word.new("lab")
+      test_word1.save
+
+      expect(Word.find("lab")).to eq test_word1
     end
   end
 
