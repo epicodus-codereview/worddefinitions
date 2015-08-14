0448142cb2bb2e0efbb030699aa2bbfdf64714d9
Alex Wait <alex@chirpify.com>
2015-08-14 16:47:54 -0700

    add integration spec and pass

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -6,23 +6,47 @@ set(:show_exceptions, false)
 describe 'word path', { type: :feature} do
   it "has links to add new word" do
     visit '/'
-    expect(page).to have content "add a new word"
+    expect(page).to have_content "Looking up a Word? Add a New Word"
   end
+  
+  it "adds new word" do
+    visit '/'
+    click_link 'Add a New Word'
+    expect(page).to have_content "Add a New Word"
+    fill_in 'word', with: 'dog'
+    click_button 'Submit'
+    expect(page).to have_content "dog"
+  end
+  
+  it 'adds a definition' do
+    visit '/'
+    click_link 'dog'
+    expect(page).to have_content "Add a New Definition"
+    fill_in 'definition', with: 'loyal creature'
+    click_button 'Submit'
+    expect(page).to have_content "loyal creature"
+  end
+  
+  
 
 end
 
-# describe 'dealership path', { type: :feature } do
-#   it 'has links to dealerships and add new dealership' do
+# # describe 'individual dealership', { type: :feature } do
+#   it 'can add an indiviual dealership' do
 #     visit '/'
-#     expect(page).to have_content "Welcome to Dealership Central See Dealership List Add New Dealership"
-#   end
-#
-#   it 'has working links' do
-#     visit '/'
-#     click_link 'See Dealership List'
-#     expect(page).to have_content 'Dealership List'
-#     click_link 'back'
 #     click_link 'Add New Dealership'
-#     expect(page).to have_content 'Submit'
+#     fill_in 'dealership', with: 'hi'
+#     click_button 'Submit'
+#     expect(page).to have_content 'hi'
+#     click_link 'hi'
+#     expect(page).to have_content 'Add a new vehicle'
+#     click_link 'Add a new vehicle'
+#     fill_in 'make', with: "Toyota"
+#     fill_in 'model', with: "Prius"
+#     fill_in 'year', with: "2000"
+#     click_button 'Add Vehicle'
+#     expect(page).to have_content "Toyota Prius 2000"
+#     click_link "Toyota Prius 2000"
+#     expect(page).to have_content "Car"
+# 
 #   end
-# end
