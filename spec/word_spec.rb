ba0cacf4d2e82e8bb24ec2043ab433e67ad510e3
Jing Wait <jingzhaowait@gmail.com>
2015-08-14 10:59:29 -0700

    add word name and word definition spec and pass

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -7,10 +7,24 @@ describe Word do
     #Word.clear
   #end
 
-  describe 'initialize' do
+  describe ('initialize') do
     it ('initialize instance for word class') do
       test_word = Word.new("apple", "a type of fruit")
       expect(test_word.name).to(eq("apple"))
+    end
+  end
+
+  describe ('#name') do
+    it("returns the name of word") do
+      test_word = Word.new("lab", "a breed of dog")
+      expect(test_word.name).to(eq("lab"))
+    end
+  end
+
+  describe ('#definition') do
+    it('returns the definition') do
+      test_word = Word.new('lab', 'a breed of dog')
+      expect(test_word.definition).to(eq("a breed of dog"))
     end
   end
 
